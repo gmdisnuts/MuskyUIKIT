c10db47dfc195aa87ffe04843dbdb5dbf73adf1e
Alex <king.zan@hotmail.com>
2021-08-07 12:03:13 -0500

    logo changes mobile etc

--- packages/pancake-uikit/src/widgets/Menu/icons/Logo.tsx
+++ packages/pancake-uikit/src/widgets/Menu/icons/Logo.tsx
@@ -10,7 +10,7 @@ const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
   const textColor = isDark ? "#FFFFFF" : "#000000";
   return (
     <Svg viewBox="0 0 160 26" {...props}>
-      <text y="20" className="logoText">BaloonSwap</text>
+      <text y="20" className="logoText">BalloonSwap</text>
       <image width="205" height="26" href={isDark ? '/images/pixel/LogoDark.gif' : '/images/pixel/LogoWhite.gif'}/>
     </Svg>
   );
